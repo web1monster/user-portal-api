54c3472ca73c6d6f6419150b08277f28c563836e
web1monster <luckyguy0716@gmail.com>
2022-01-10 17:32:12 -0800

    feat: add seed function which will populate database automatically

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { SeedService } from './seed.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [SeedService]
+  providers: [SeedService],
+  imports: [UserModule],
 })
 export class SeedModule {}

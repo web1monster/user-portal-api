c4c0fd0e834455ce23d8beaa00af7eb63f6ad8aa
web1monster <luckyguy0716@gmail.com>
2022-01-10 17:36:48 -0800

    feat: add healthy api to get server status

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -8,7 +8,7 @@ export class SeedService {
   constructor(private readonly userService: UserService) {}
 
   async startSeed(): Promise<void> {
-    for (let user of seedUsers) {
+    for (const user of seedUsers) {
       // Ignore id. id will be prefilled by the database automatically.
       await this.userService.add({ ...user, id: null });
     }

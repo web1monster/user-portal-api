e34e5449b7676209da62ce295b8c2d1abcc44f51
web1monster <luckyguy0716@gmail.com>
2022-01-10 20:34:57 -0800

    test: unit test for the user controller

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -6,6 +6,7 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { seedUsers } from '../seed/seed-user';
 import { getFromDto } from '../common/utils/repository.util';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UserService', () => {
   let service: UserService;
@@ -25,10 +26,16 @@ describe('UserService', () => {
     service = module.get<UserService>(UserService);
     repo = module.get<Repository<User>>(getRepositoryToken(User));
 
-    jest.spyOn(repo, 'findOne').mockResolvedValue(null);
     jest
       .spyOn(repo, 'save')
       .mockImplementation((user: User) => getFromDto(user, new User()));
+    jest
+      .spyOn(repo, 'find')
+      .mockResolvedValue(seedUsers.map((u) => getFromDto(u, new User())));
+    jest.spyOn(repo, 'findOne').mockImplementation((condition) => {
+      const found = seedUsers.find((u) => u.userId === condition.userId);
+      return found ? getFromDto(found, new User()) : null;
+    });
   });
 
   it('should be defined', () => {
@@ -37,6 +44,9 @@ describe('UserService', () => {
 
   it('should create new user with the payload', async () => {
     const payload = seedUsers[0];
+
+    jest.spyOn(repo, 'findOne').mockResolvedValueOnce(null);
+
     const createdUser = await service.add(payload);
     expect(createdUser.userId).toEqual(payload.userId);
     expect(createdUser.title).toEqual(payload.title);
@@ -46,6 +56,7 @@ describe('UserService', () => {
 
   it('should return existing user when userId is already exists', async () => {
     const payload = seedUsers[0];
+
     jest
       .spyOn(repo, 'findOne')
       .mockResolvedValueOnce(getFromDto(payload, new User()));
@@ -53,5 +64,29 @@ describe('UserService', () => {
     const createdUser = await service.add(payload);
     expect(createdUser.userId).toEqual(payload.userId);
     expect(repo.save).not.toBeCalled();
+  });
+
+  it('should return all users using findAll function', async () => {
+    const all = await service.findAll();
+    expect(all.length).toEqual(10);
+  });
+
+  it('should return specific user based on the userId', async () => {
+    const found = await service.findUserByUserId(5);
+    expect(found.userId).toBe(5);
+    expect(found.title).toBe('nesciunt quas odio');
+    expect(found.body).toBe(
+      'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque',
+    );
+  });
+
+  it('should return 404 not found error when user id does not exists', async () => {
+    try {
+      await service.findUserByUserId(165);
+    } catch (e) {
+      expect(e).toStrictEqual(
+        new NotFoundException('User does not exists for that userId - 165'),
+      );
+    }
   });
 });

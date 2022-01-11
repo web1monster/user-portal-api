c56de8e5f81cd1c4659f2abcd97ef0ea61dd7d9d
web1monster <luckyguy0716@gmail.com>
2022-01-10 18:44:28 -0800

    test: unit test for the user service function

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,34 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { seedUsers } from '../seed/seed-user';
+import { getFromDto } from '../common/utils/repository.util';
 
 describe('UserService', () => {
   let service: UserService;
+  let repo: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    })
-      .useMocker((token) => {
-        if (token === 'UserRepository') {
-          return {
-            findOne: jest
-              .fn()
-              .mockImplementation((condition) =>
-                seedUsers.find((u) => u.userId === condition.userId),
-              ),
-            save: jest.fn().mockImplementation((user: User) => user),
-          };
-        }
-      })
-      .compile();
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
 
     service = module.get<UserService>(UserService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
+
+    jest.spyOn(repo, 'findOne').mockResolvedValue(null);
+    jest
+      .spyOn(repo, 'save')
+      .mockImplementation((user: User) => getFromDto(user, new User()));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create new user with the payload', async () => {
+    const payload = seedUsers[0];
+    const createdUser = await service.add(payload);
+    expect(createdUser.userId).toEqual(payload.userId);
+    expect(createdUser.title).toEqual(payload.title);
+    expect(createdUser.body).toEqual(payload.body);
+    expect(repo.save).toBeCalled();
+  });
+
+  it('should return existing user when userId is already exists', async () => {
+    const payload = seedUsers[0];
+    jest
+      .spyOn(repo, 'findOne')
+      .mockResolvedValueOnce(getFromDto(payload, new User()));
+
+    const createdUser = await service.add(payload);
+    expect(createdUser.userId).toEqual(payload.userId);
+    expect(repo.save).not.toBeCalled();
   });
 });

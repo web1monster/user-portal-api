4e8bc165c18f8d1c6c41bac59636c774a87c36a0
web1monster <luckyguy0716@gmail.com>
2022-01-12 22:08:45 -0800

    feat: enable cors for the api

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors();
+
   const seedService = app.get(SeedService);
 
   await seedService.startSeed();
